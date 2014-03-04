0d7f5a115d00609b90bdd7b394144ecf1fb2492e
Carlos Aguado Sanchez <carlos.aguado@epfl.ch>
2014-03-04 10:26:15 +0100

    Upgrade ucernvm to v1.17-3
    Signed-off-by: Carlos Aguado Sanchez <carlos.aguado@epfl.ch>

--- src/get_ucernvm.sh
+++ src/get_ucernvm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="1.16-3"
+VERSION="1.17-3"
 UCVM_REPO="http://cernvm.cern.ch/releases"
 UCVM_DIR="ucernvm-images.${VERSION}.cernvm.x86_64"
 UCVM_IMAGE="ucernvm-prod.${VERSION}.cernvm.x86_64.hdd"
@@ -9,3 +9,4 @@ UCVM_BOOT="ucernvm.vdi"
 [ ! -f ${UCVM_IMAGE} ] && wget ${UCVM_REPO}/${UCVM_DIR}/${UCVM_IMAGE} -O ${UCVM_IMAGE}
 qemu-img convert -O ${FORMAT} ${UCVM_IMAGE} ${UCVM_BOOT}
 chmod 0644 ${UCVM_BOOT}
+rm -f ${UCVM_IMAGE}
